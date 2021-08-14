e19234f75efb3d7f82e1ecd9ba1a0a337129f96b
Nujood <shawar.nujood@gmail.com>
2021-08-14 20:49:12 +0300

    handling validation error by `GraphQLFormattedError`

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
-import 'reflect-metadata'
-import { ApolloServer } from "apollo-server-express";
-import Express from 'express'
-import { buildSchema } from "type-graphql";
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import Express from 'express';
+import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 import { RegisterResolver } from './modules/user/Register';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
     resolvers: [RegisterResolver],
+    validate: true
   });
-  
-  const apolloServer = new ApolloServer({ schema })
-  const app = Express()
+
+  const apolloServer = new ApolloServer({
+    schema,
+    formatError: (error: GraphQLError): GraphQLFormattedError => {
+      if (error && error.extensions) {
+        error.extensions.code = 'GRAPHQL_VALIDATION_FAILED';
+      }
+      return error;
+    },
+  });
+  const app = Express();
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app })
-  
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log(`server is running on port 4000`);
-  })
-}
+  });
+};
 
-main()
+main();
