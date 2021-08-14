3641b2a6f32c63819a7430875df5b34c93dfbbc0
Nujood <shawar.nujood@gmail.com>
2021-08-14 23:33:53 +0300

    Setup express-session with redis as a storage

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import 'reflect-metadata';
+import { config } from 'dotenv';
 import { ApolloServer } from 'apollo-server-express';
 import Express from 'express';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
-import { RegisterResolver } from './modules/user/Register';
 import { GraphQLError, GraphQLFormattedError } from 'graphql';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import cors from 'cors';
+import { RegisterResolver } from './modules/user/Register';
+import { LoginResolver } from './modules/user/Login';
+import { redis } from './redis';
+
+config();
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
-    validate: true
+    resolvers: [RegisterResolver, LoginResolver],
+    validate: true,
   });
 
   const apolloServer = new ApolloServer({
@@ -22,8 +30,27 @@ const main = async () => {
       }
       return error;
     },
+    context: ({ req }: any) => ({ req }),
   });
+
   const app = Express();
+  const RedisStore = connectRedis(session);
+
+  app.use(cors());
+  app.use(
+    session({
+      store: new RedisStore({ client: redis }),
+      secret: process.env.SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: process.env.NODE_ENV === 'production', // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie
+        maxAge: 1000 * 60 * 10, // session max age in miliseconds
+      },
+    }),
+  );
+
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
 
